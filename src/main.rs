32682484d274f12bbb2229eb262923b367c72ec3
Yokoyama Kosuke <a9500186@gmail.com>
2024-04-24 19:04:34 +0900

    aaa

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,38 @@
-// fn hello() -> String {
-//     "Hello, world!".to_string()
-// }
-// fn main() {
-//     println!("{}", hello());
-// }
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     #[test]
-//     fn test_hello() {
-//         assert_eq!(hello(), "Hello, world!");
-//     }
-// }
-use clap::{Parser, ValueEnum};
-
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Compress or Decompress file name
-    #[arg(value_name = "FILE")]
-    file: String,
-
-    /// Multiple File Designation
-    #[arg(last=true)]
-    multiple_files: Vec<String>,
-
-    /// Whether to decompress the input
-    #[arg(short, long)]
-    decompress: bool,
-
-    /// Put passwords on compressed files
-    #[arg(short, long)]
-    encrypt: bool,
-
-    #[arg(short, long)]
-    #[clap(value_enum, default_value_t=Format::Zip)]
-    format: Format,
-
-    /// Whether to recursively greet
-    #[arg(short, long)]
-    recursive: bool,
-
-    /// Verbose mode
-    #[arg(short, long)]
-    verbose: bool,
-
-    /// Number of compressions to perform
-    #[arg(short, long, default_value = "6")]
-    count: u32,
-}
-
-#[derive(ValueEnum, Clone, Debug)]
-enum Format {
-    Gzip,
-    Zip,
-}
-
 fn hello() -> String {
     "Hello, world!".to_string()
 }
-
 fn main() {
-    let _args = Args::parse();
-
-    println!("Hello, world!");
     println!("{}", hello());
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn test_hello() {
         assert_eq!(hello(), "Hello, world!");
     }
 }
+// use clap::Parser;
+// use cli::CliOpts;
+
+// mod cli;
+
+// fn main() {
+//     let _opts = CliOpts::parse();
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use std::path::PathBuf;
+//     use cli::RunMode;
+//     use super::*;
+
+//     #[test]
+//     fn test_run() {
+//         let opts = CliOpts::parse_from(&["totebag_test", "-o", "test.zip", "src", "LICENSE", "README.md", "Cargo.toml"]);
+//         assert_eq!(opts.mode, RunMode::Auto);
+//         assert_eq!(opts.output, Some(PathBuf::from("test.zip")));
+//         assert_eq!(opts.args.len(), 4);
+//         assert_eq!(opts.args, vec![PathBuf::from("src"), PathBuf::from("LICENSE"), PathBuf::from("README.md"), PathBuf::from("Cargo.toml")]);
+//     }
+// }
