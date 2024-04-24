80b647b7460823d54a8218c3798b4c626b0ffbfb
Yokoyama Kosuke <a9500186@gmail.com>
2024-04-24 18:59:49 +0900

    aaa

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn hello() -> String {
+// fn hello() -> String {
 //     "Hello, world!".to_string()
 // }
 // fn main() {
@@ -12,27 +12,68 @@ fn hello() -> String {
 //         assert_eq!(hello(), "Hello, world!");
 //     }
 // }
-use clap::Parser;
-use cli::CliOpts;
+use clap::{Parser, ValueEnum};
 
-mod cli;
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Compress or Decompress file name
+    #[arg(value_name = "FILE")]
+    file: String,
+
+    /// Multiple File Designation
+    #[arg(last=true)]
+    multiple_files: Vec<String>,
+
+    /// Whether to decompress the input
+    #[arg(short, long)]
+    decompress: bool,
+
+    /// Put passwords on compressed files
+    #[arg(short, long)]
+    encrypt: bool,
+
+    #[arg(short, long)]
+    #[clap(value_enum, default_value_t=Format::Zip)]
+    format: Format,
+
+    /// Whether to recursively greet
+    #[arg(short, long)]
+    recursive: bool,
+
+    /// Verbose mode
+    #[arg(short, long)]
+    verbose: bool,
+
+    /// Number of compressions to perform
+    #[arg(short, long, default_value = "6")]
+    count: u32,
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum Format {
+    Gzip,
+    Zip,
+}
+
+fn hello() -> String {
+    "Hello, world!".to_string()
+}
 
 fn main() {
-    let _opts = CliOpts::parse();
+    let _args = Args::parse();
+
+    println!("Hello, world!");
+    println!("{}", hello());
 }
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
-    use cli::RunMode;
     use super::*;
 
     #[test]
-    fn test_run() {
-        let opts = CliOpts::parse_from(&["totebag_test", "-o", "test.zip", "src", "LICENSE", "README.md", "Cargo.toml"]);
-        assert_eq!(opts.mode, RunMode::Auto);
-        assert_eq!(opts.output, Some(PathBuf::from("test.zip")));
-        assert_eq!(opts.args.len(), 4);
-        assert_eq!(opts.args, vec![PathBuf::from("src"), PathBuf::from("LICENSE"), PathBuf::from("README.md"), PathBuf::from("Cargo.toml")]);
+    fn test_hello() {
+        assert_eq!(hello(), "Hello, world!");
     }
 }
