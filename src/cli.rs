e59604724fd7fd7f55b37781a8af7ea42bd0299d
Yokoyama Kosuke <a9500186@gmail.com>
2024-04-30 17:39:26 +0900

    update  cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -1,28 +1,29 @@
-pub struct CliOpts {
-    #[arg(short, long, default_value_t = RunMode::Auto,
-        value_name = "MODE", required = false,
-        ignore_case = true, value_enum, help = "Mode of operation.")] 
-    pub mode: RunMode,
-    #[arg(short, long, default_value = ".", value_name = "DEST", required = false, 
-          help = "Destination of the extraction results.")]
-    pub dest: Option<PathBuf>,
-    #[arg(short, long, default_value = "cpt.zip", value_name = "OUTPUT", required = false,
-          help = "Output file for the archive.")]
-    pub output: Option<PathBuf>,
-    #[arg(short, long = "no-recursive", help = "No recursive mode.", default_value_t = false)] 
-    pub no_recursive: bool,
-    #[arg(short, long, help = "Display verbose output.", default_value_t = false)]
-    pub verbose: bool,
-    #[arg(value_name = "ARGUMENTS", help = "List of files or directories to be processed.",
-          long_help = "extract mode: archive files to be extracted.
-    archive mode: files to be archived.
-    auto mode: if the arguments have archive files, it will extract them. Otherwise, it will archive the files.")]
-      pub args: Vec<PathBuf>,
-    }
+use std::path::PathBuf;
+use clap::{Parser, ValueEnum};
 
-    #[derive(Debug, Clone, ValueEnum, PartialEq)] 
-    pub enum RunMode {
-        Auto,
-        Archive,
-        Extract,
-    }
+#[derive(Parser, Debug)]
+#[clap(
+    version, author, about,
+    arg_required_else_help = true,
+)]
+pub struct CliOpts {
+    #[clap(short = 'm', long = "mode", default_value_t = RunMode::Auto, value_name = "MODE", required = false, ignore_case = true, value_enum, help = "Mode of operation.")]
+    pub mode: RunMode,
+    #[clap(short = 'd', long = "dest", default_value = ".", value_name = "DEST", required = false, help = "Destination of the extraction results.")]
+    pub dest: Option<PathBuf>,
+    #[clap(short = 'o', long = "output", default_value = "totebag.zip", value_name = "OUTPUT", required = false, help = "Output file for the archive.")]
+    pub output: Option<PathBuf>,
+    #[clap(short = 'n', long = "no-recursive", help = "No recursive mode.", default_value_t = false)]
+    pub no_recursive: bool,
+    #[clap(short = 'v', long = "verbose", help = "Display verbose output.", default_value_t = false)]
+    pub verbose: bool,
+    #[clap(value_name = "ARGUMENTS", help = "List of files or directories to be processed.")]
+    pub args: Vec<PathBuf>,
+}
+
+#[derive(Debug, Clone, ValueEnum, PartialEq)]
+pub enum RunMode {
+    Auto,
+    Archive,
+    Extract,
+}
