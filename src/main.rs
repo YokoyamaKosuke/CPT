76c3c964ed960ba38f0da20506139cabbe8f12e1
Yokoyama Kosuke <a9500186@gmail.com>
2024-05-11 15:34:46 +0900

    update test

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,39 @@
-// fn hello() -> String {
-//     "Hello, world!".to_string()
-// }
-// fn main() {
-//     println!("{}", hello());
-// }
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     #[test]
-//     fn test_hello() {
-//         assert_eq!(hello(), "Hello, world!");
-//     }
-// }
-
-use clap::Parser;
-use cli::CliOpts;
-
-mod cli;
-
-fn main() {
-    let _opts = CliOpts::parse();
+fn hello() -> String {
+    "Hello, world!".to_string()
 }
-
+fn main() {
+    println!("{}", hello());
+}
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
-    use cli::RunMode;
     use super::*;
-
     #[test]
-    fn test_run() {
-        let opts = CliOpts::parse_from(&["cpt_test", "-o", "test.zip", "src", "LICENSE", "README.md", "Cargo.toml"]);
-        assert_eq!(opts.mode, RunMode::Auto);
-        assert_eq!(opts.output, Some(PathBuf::from("test.zip")));
-        assert_eq!(opts.args.len(), 4);
-        assert_eq!(opts.args, vec![PathBuf::from("src"), PathBuf::from("LICENSE"), PathBuf::from("README.md"), PathBuf::from("Cargo.toml")]);
+    fn test_hello() {
+        assert_eq!(hello(), "Hello, world!");
     }
 }
+
+// use clap::Parser;
+// use cli::CliOpts;
+
+// mod cli;
+
+// fn main() {
+//     let _opts = CliOpts::parse();
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use std::path::PathBuf;
+//     use cli::RunMode;
+//     use super::*;
+
+//     #[test]
+//     fn test_run() {
+//         let opts = CliOpts::parse_from(&["cpt_test", "-o", "test.zip", "src", "LICENSE", "README.md", "Cargo.toml"]);
+//         assert_eq!(opts.mode, RunMode::Auto);
+//         assert_eq!(opts.output, Some(PathBuf::from("test.zip")));
+//         assert_eq!(opts.args.len(), 4);
+//         assert_eq!(opts.args, vec![PathBuf::from("src"), PathBuf::from("LICENSE"), PathBuf::from("README.md"), PathBuf::from("Cargo.toml")]);
+//     }
+// }
